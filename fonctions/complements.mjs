988d3bbb6f6d3c267f909b68cbeb7eb9a77ea343
Abdoulaye7 <momo.diengdiallo18@gmail.com>
2024-10-21 00:53:16 +0200

    modification pour l'indication et le prix

--- fonctions/complements.mjs
+++ fonctions/complements.mjs
@@ -1,14 +1,14 @@
 
 function complementsPremiersPrixPour(complements, indication, prix){
-    return complements.filter(c => c.complements.indexOf(indication) !== -1 && c.prix < prix);
+    return complements.filter(c => c.indications.indexOf(indication) !== -1 && c.prix < prix);
 }
 
 function siropSontPremierPrix(complements){
-    return complements.filter( c => c.type === 'sirop').every(c => c.prix < prix);
+    return complements.filter( c => c.type === 'sirop').every(c => c.prix <= 5);
 }
 
 function nomsComplementsPour(complements, indication){
-    return complements.filter(c => c.complements.indexOf(indication) !== -1 ).map(c => c.nom);
+    return complements.filter(c => c.indications.indexOf(indication) !== -1 ).map(c => c.nom);
 }
 
 function imprimerNomsComplementsIterateur(it){
