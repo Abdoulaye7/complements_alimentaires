379c5d1ca340622b5410aa87e387739270896596
Abdoulaye7 <momo.diengdiallo18@gmail.com>
2024-10-21 02:05:25 +0200

    ajout du routeur

---
+++ routes/complementpour.mjs
@@ -0,0 +1,46 @@
+
+
+import {express} from 'express';
+
+
+
+function recupererPrixMinimum(req,res,next){
+    if(req.query.prixMin && typeof req.query.prixMin === 'string'){
+        res.locals.prixMin = req.query.prixMin;
+    }else{
+        res.status(400).send('Mauvaise requête');
+    }
+    next();
+}
+
+function recupererIndicationChemin(req,res,next){
+    if(req.param.indication && typeof req.param.indication === 'string'){
+         res.locals.indication = req.param.indication;
+    }else{
+        res.status(400).send('Mauvaise requête');
+    }
+    next();
+
+}
+
+async function renvoyerComplements(req,res){
+   try{
+        const complements = await req.app.locals.db.collection("catalogue")
+    .find({indication: res.locals.indication, prix: { $lte: res.locals.prixMin }})
+    .toArray();
+    res.json(complements);
+
+
+   }catch(err){
+    res.status(500).send();
+   }
+} 
+const catalogueRouteur = express.Router(); 
+
+catalogueRouteur.get('/complements/indication/:indication',recupererIndicationChemin,
+    recupererPrixMinimum, renvoyerComplements);
+
+ catalogueRouteur.get('/complements',
+        recupererIndication, recupererPrixMinimum, renvoyerComplements);
+
+    export {catalogueRouteur}
